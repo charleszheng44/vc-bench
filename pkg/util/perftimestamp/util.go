bf64ccfe5a329fae5572242e32c16fdaad4959d8
chao zheng <charleszheng44@gmail.com>
2020-04-06 13:00:47 -0700

    fix argument bug

--- pkg/util/perftimestamp/util.go
+++ pkg/util/perftimestamp/util.go
@@ -1,16 +1,15 @@
 package perftimestamp
 
 import (
+	"k8s.io/api/core/v1"
 	"strconv"
-
-	tenancyv1alpha1 "github.com/kubernetes-sigs/multi-tenancy/incubator/virtualcluster/pkg/apis/tenancy/v1alpha1"
 )
 
-func AnnotateTimestamp(vc *tenancyv1alpha1.Virtualcluster, key string, timestamp int64) {
-	if len(vc.Annotations) == 0 {
-		vc.Annotations = make(map[string]string)
+func AnnotateTimestamp(vPod *v1.Pod, key string, timestamp int64) {
+	if len(vPod.Annotations) == 0 {
+		vPod.Annotations = make(map[string]string)
 	}
-	if _, exist := vc.Annotations[key]; !exist {
-		vc.Annotations[key] = strconv.Itoa(int(timestamp))
+	if _, exist := vPod.Annotations[key]; !exist {
+		vPod.Annotations[key] = strconv.Itoa(int(timestamp))
 	}
 }
