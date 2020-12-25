baaaad869603008ef03eb09070ed4276186bd4f8
chao zheng <charleszheng44@gmail.com>
2020-12-25 12:26:02 -0800

    add dist py

---
+++ data/20dws200uws/100tenants10000pods/creation_time_dist.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+
+creation_ts = []
+with open("100tenants10000pods.log") as fp:
+    next(fp)
+    lines = fp.readlines()
+    for line in lines:
+        tokens = line.split(",")
+        creation_ts.append(int(tokens[3])) 
+
+plt.hist(creation_ts, edgecolor='black', linewidth=1)
+plt.show()
